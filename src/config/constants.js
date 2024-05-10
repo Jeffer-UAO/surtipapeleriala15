47273484095578ea36b869d1997e4c09f5a471da
JefferDevop <somadesarrollo@gmail.com>
2024-05-10 08:30:21 -0500

    Cambio numeros celulares 2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-// export const BASE_API = `http://mihogardistribuciones.catalogointeractivo.com.co:9000`
-
-export const BASE_API = NEXT_PUBLIC_API
-=======
 export const BASE_API = `https://surtipapeleriala15.catalogointeractivo.com.co`;
->>>>>>> 1c1d1360eaaa077709d5b8932b5b4da238abee03
-
 export const SEARCH_HISTORY = "search-history";
 export const TOKEN = "token";
 export const CART = "cart";
